02f42ba263e63cda7fdf3f45e50ba37fe8a279f5
aurelien <aurelien.martin@kbrwadventure.com>
2023-04-17 12:20:22 +0200

    Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy({ "input/robots.txt": "/robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "input/_includes/images": "/images" });
 
   return {
     dir: {
