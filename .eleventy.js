6f090f582f5374d22296854e98f5a0ed42611082
aurelien <aurelien.martin@kbrwadventure.com>
2024-10-06 09:40:47 +0200

    Upgrade to 11ty 3.0.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
+export default function (eleventyConfig) {
+  eleventyConfig.addBundle("css");
+
+  eleventyConfig.addPlugin(syntaxHighlight);  
   eleventyConfig.addPassthroughCopy({ "input/robots.txt": "/robots.txt" });
   eleventyConfig.addPassthroughCopy({ "input/_includes/images": "/images" });
+  eleventyConfig.addPassthroughCopy({ "input/_includes/fonts": "/fonts" });
 
   return {
     dir: {
